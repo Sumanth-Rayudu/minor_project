8ef52e07d33ab468a6ac67cb79963fcd3cfa0c6c
Sumanth-Rayudu <109367584+Sumanth-Rayudu@users.noreply.github.com>
2023-03-26 16:44:40 +0530

    Update new proj minor.ino

--- new proj minor.ino
+++ new proj minor.ino
@@ -25,51 +25,42 @@ void setup() {
   mcp2515.setBitrate(CAN_125KBPS,MCP_8MHZ);
   mcp2515.setNormalMode();
 
-  HUM.can_id = 0x100;
+  HUM.can_id = 0xaa;
   HUM.can_dlc = 2;
-  TEMP.can_id = 0x101;
+  TEMP.can_id = 0xbb;
   TEMP.can_dlc = 2;
-  SMOKE.can_id = 0x102;
+  SMOKE.can_id = 0xcc;
   SMOKE.can_dlc = 3;
-  LDR.can_id = 0x103;
+  LDR.can_id = 0xdd;
   LDR.can_dlc = 3;
-  FIRE.can_id = 0x104;
+  FIRE.can_id = 0xee;
   FIRE.can_dlc = 1;
 }
 
 void loop() {
   int HUMD = dht.readHumidity();
-  HUM.can_id = 0x100;
   HUM.data[0] = HUMD;
   mcp2515.sendMessage(&HUM);
 
-  int TEM = dht.readTemperature();
-  TEMP.can_id = 0x101;
-  TEMP.data[0] = TEM;
+  int TEMP = dht.readTemperature(); // Fix: Changed variable name to TEMP
+  TEMP.data[0] = TEMP; // Fix: Changed array index to 0
   mcp2515.sendMessage(&TEMP);
-
-  int SMO = analogRead(A0);
-  int SMO_high = (SMO >> 8) & 0xFF;
-  int SMO_low = SMO & 0xFF;
-  SMOKE.can_id = 0x102;
-  SMOKE.data[0] = SMO_high;
-  SMOKE.data[1] = SMO_low;
-  SMOKE.data[2] = FIRalert;
+  
+  int SMO = analogRead(A1);
+  SMOKE.data[0] = SMO >> 8; // Send the high byte
+  SMOKE.data[1] = SMO & 0xFF; // Send the low byte
   mcp2515.sendMessage(&SMOKE);
 
-  int LDR_val = analogRead(A1);
-  int LDR_high = (LDR_val >> 8) & 0xFF;
-  int LDR_low = LDR_val & 0xFF;
-  LDR.can_id = 0x103;
-  LDR.data[0] = LDR_high;
-  LDR.data[1] = LDR_low;
-  LDR.data[2] = FIRalert;
+  int LDRv = analogRead(A0);
+  LDR.data[0] = LDRv >> 8; // Send the high byte
+  LDR.data[1] = LDRv & 0xFF; // Send the low byte
   mcp2515.sendMessage(&LDR);
 
-  int FIR_val = analogRead(Fir);
-  FIRE.can_id = 0x104;
-  FIRE.data[0] = FIR_val;
+  int FIRv = analogRead(Fir);
+  if (FIRv > FIRalert) {
+    FIRE.data[0] = 0x01; // Send 0x01 if fire is detected
+  } else {
+    FIRE.data[0] = 0x00; // Send 0x00 if no fire is detected
+  }
   mcp2515.sendMessage(&FIRE);
-
-  delay(1000);
 }
