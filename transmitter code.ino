5bef879d1399635fcb1d7c53afc64fe78f4195dd
Sumanth-Rayudu <109367584+Sumanth-Rayudu@users.noreply.github.com>
2023-05-03 23:49:41 +0530

    Update transmitter code.ino

--- transmitter code.ino
+++ transmitter code.ino
@@ -2,7 +2,7 @@
 #include <mcp2515.h>
 #include <DHT.h>
 
-#define DHTPIN A3
+#define DHTPIN 4
 #define DHTTYPE DHT11
 
 struct can_frame HUM;
@@ -11,18 +11,25 @@ struct can_frame SMOKE;
 struct can_frame LDR;
 struct can_frame FIRE;
 
-int Fir = A2;
-int FIRalert = 1;
+const int gaspin = A0;
+const int LDR_pin = A5;
+const int IR_digi = 7;
+
+
 
 MCP2515 mcp2515(10);
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
+
+  pinMode(gaspin,INPUT);
+  pinMode(IR_digi,INPUT);
+  Serial.begin(9600);
   SPI.begin();
   dht.begin();
   
   mcp2515.reset();
-  mcp2515.setBitrate(CAN_125KBPS,MCP_8MHZ);
+  mcp2515.setBitrate(CAN_500KBPS,MCP_8MHZ);
   mcp2515.setNormalMode();
 
   HUM.can_id = 0xaa;
@@ -38,29 +45,59 @@ void setup() {
 }
 
 void loop() {
+
   int HUMD = dht.readHumidity();
+  HUM.can_id = 0xaa;
   HUM.data[0] = HUMD;
   mcp2515.sendMessage(&HUM);
-
-  int TEMP = dht.readTemperature(); // Fix: Changed variable name to TEMP
-  TEMP.data[0] = TEMP; // Fix: Changed array index to 0
+  Serial.print("humidity : ");
+  Serial.println(HUMD);
+  delay(300);
+    
+  int TEM = dht.readTemperature();
+  TEMP.can_id = 0xbb;
+  TEMP.data[0] = TEM;
   mcp2515.sendMessage(&TEMP);
+  Serial.print("Temperature : ");
+  Serial.println(TEM);
+   delay(300);
+
   
-  int SMO = analogRead(A1);
-  SMOKE.data[0] = SMO >> 8; // Send the high byte
-  SMOKE.data[1] = SMO & 0xFF; // Send the low byte
+  int smokeVal = analogRead(gaspin);
+  SMOKE.can_id = 0xcc;
+  if(smokeVal > 40){
+    Serial.println(smokeVal);
+
+  }
+  SMOKE.data[0] = smokeVal & 0xFF;
+  SMOKE.data[1] = (smokeVal >> 8) & 0xFF;
   mcp2515.sendMessage(&SMOKE);
+  Serial.print("smoke : ");
+  Serial.println(smokeVal);
+   delay(300);
 
-  int LDRv = analogRead(A0);
-  LDR.data[0] = LDRv >> 8; // Send the high byte
-  LDR.data[1] = LDRv & 0xFF; // Send the low byte
+  
+  int ldrVal = analogRead(LDR_pin);
+  LDR.can_id = 0xdd;
+  LDR.data[0] = ldrVal & 0xFF;
+  LDR.data[1] = (ldrVal >> 8) & 0xFF;
   mcp2515.sendMessage(&LDR);
-
-  int FIRv = analogRead(Fir);
-  if (FIRv > FIRalert) {
-    FIRE.data[0] = 0x01; // Send 0x01 if fire is detected
-  } else {
-    FIRE.data[0] = 0x00; // Send 0x00 if no fire is detected
+  Serial.print("light intensity : ");
+  Serial.println(ldrVal);
+  delay(300);
+  
+  int irVal = digitalRead(IR_digi);
+  FIRE.can_id = 0xee;
+  if(irVal==0){
+    FIRE.data[0] = 1;
+    Serial.println("Fire Detected");
+  }
+  else {
+    FIRE.data[0] = 0;
+    Serial.println("No Fire Detected");
   }
   mcp2515.sendMessage(&FIRE);
+  delay(300);
+  
+  delay(1000); // wait a second between readings
 }
